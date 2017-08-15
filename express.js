7fc85dd95ba64a057bba34dcf762039ee635b235
jakedub <jacob.w.moore@gmail.com>
2017-08-15 16:22:09 -0400

    closer

--- express.js
+++ express.js
@@ -5,15 +5,21 @@ const mustacheExpress = require("mustache-express");
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views")
 app.set("view engine", "mustache")
+app.use(express.static("public"))
 
-const data = {
-users: [
-    {id: "", "username": "", "name": "", "avatar": "", "email": "", },
-  ]
-};
+app.get('/user', function (req, res) {
+  res.render("user", realData);
+});
 
-app.get("/robot", (req, res) => {
-  res.render("robot", realData);
+function hire(){
+  for (let i=0; i<realData.users.job.length; i++)
+  if (realData[i].users.job === "null"){
+    return "Available for hire";
+  }
+}
+
+app.get('/template', function (req, res) {
+  res.render("template", realData);
 });
 
 app.listen(3000, function () {
